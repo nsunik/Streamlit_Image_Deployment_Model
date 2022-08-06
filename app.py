2e0b637f12469564946bf08e8543915132cac008
N.Sunik <71277942+nsunik@users.noreply.github.com>
2022-08-06 11:55:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import numpy as np
 #st.set_option('deprecation.showfileUploaderEncoding', False)
 def import_and_predict(image_data, model):
     
-        size = (224,224)    
+        size = (64,64)    
         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
         image = np.asarray(image)
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
